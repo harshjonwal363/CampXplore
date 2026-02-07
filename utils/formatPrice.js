d766237b4b89a822b40191d173036611f5cbc843
harshjonwal363 <harshjonwal363@gmail.com>
2026-02-07 13:15:00 +0530

    Add files via upload

---
+++ utils/formatPrice.js
@@ -0,0 +1,7 @@
+module.exports = function formatPriceINR(amount) {
+    return new Intl.NumberFormat('en-IN', {
+        style: 'currency',
+        currency: 'INR',
+        maximumFractionDigits: 0
+    }).format(amount);
+};
